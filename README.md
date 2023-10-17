e915e401632d71a6c00db5eac30a35881933bf9d
marick2003 <mmaarriicckk@gmail.com>
2023-10-17 18:19:19 +0800

    調整router (未完

--- README.md
+++ README.md
@@ -5,3 +5,14 @@ This template should help get you started developing with Vue 3 in Vite. The tem
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+## 參考套件
+Vite  (本機開發使用，取代vue cli)
+Pinia (取代Vuex狀態管理)
+Vue Router (與之前專案相同)
+Vue Use (類似語法糖的lib)
+Vue i18n (多國語系套件)
+VeeValidate (欄位驗證套件)
+TailWindCSS (inline class使用)
+Element Plus Native UI (日曆、彈窗、layout統整的套件)
+Axios (API請求套件)
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import TestPage from '@/views/TestPage.vue'
-const router = createRouter({
+
+  let routes = [];
+  routes = routes_pc;
+  if (
+    navigator.userAgent.match(
+      /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBroswer|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
+    )
+  ) {
+    routes = routes_h5;
+  }
+  let routes_pc=[
+    {
+        path: '/',
+        name: 'TestPage',
+        component: TestPage,
+
+    }
+  ]
+  const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
       {
@@ -10,6 +28,7 @@ const router = createRouter({
       }
     ]
   })
+
   router.beforeEach( (to,from)=>{
   
   });
