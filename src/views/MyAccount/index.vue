805c1af29a85b9dbf96170fac666a840f3a071ad
marick2003 <mmaarriicckk@gmail.com>
2023-10-20 18:25:48 +0800

    調整樣式

--- src/views/MyAccount/index.vue
+++ src/views/MyAccount/index.vue
@@ -1,52 +1,53 @@
 <template>
-    <div class="common-layout h-full">
-      <el-container class="h-full">
-        <el-aside width="265px" >
-          
-          <el-menu class="el-menu-vertical-demo" >
-            <div class=" text-2xl font-bold">我的账户</div>
-             <el-menu-item>
-                控制面板 
-             </el-menu-item>
-             <el-menu-item>
-                个人档案  
-             </el-menu-item>
-             <el-menu-item>
-                账户记录   
-             </el-menu-item>
-             <el-menu-item>
-                设置   
-             </el-menu-item>
-             <div class="">
+  <div class="common-layout h-full">
+    <el-container class="h-full">
+      <el-aside width="265px">
+        <el-menu router class="el-menu-vertical-demo">
+          <div class="title">我的账户</div>
 
-             </div>
-          </el-menu>
-        </el-aside>
-        <el-main class=" h-full">
-              <RouterView></RouterView>
-        </el-main>
-      </el-container>
+          <a href="/myaccount">
+            <el-menu-item> 控制面板 </el-menu-item>
+          </a>
+          <a href="/Profile">
+            <el-menu-item> 个人档案 </el-menu-item>
+          </a>
+          <a href="/Statement">
+            <el-menu-item> 账户记录 </el-menu-item>
+          </a>
+          <a href="/Settings">
+            <el-menu-item> 设置 </el-menu-item>
+          </a>
+        </el-menu>
+      </el-aside>
+      <el-main class="h-full">
+        <RouterView></RouterView>
+      </el-main>
+    </el-container>
   </div>
 </template>
-<style scoped>
-  .common-layout{
-    position: relative;
-    top:0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: #fff;
-  }
-  .el-aside{
-    height: 80vh;
-    background-color: azure;
-  }
-  .el-menu{
-    border-right: 0;
-  }
+<script setup></script>
+<style scoped lang="scss">
+.common-layout {
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+}
+.el-aside {
+  height: 80vh;
+  background-color: #fff;
+  border: 1px solid #ccc;
+}
+.el-menu {
+  border-right: 0;
+}
+.el-menu-item {
+  border-bottom: 1px solid #ccc;
+}
+.title {
+  padding: 20px;
+  border-bottom: 1px solid #ccc;
+}
 </style>
-
-
-
-
-
